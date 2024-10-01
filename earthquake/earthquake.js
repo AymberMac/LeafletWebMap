db6d7945994acd9a54bf1b10b4a9ffcb544c6a01
Aymber McElroy <aymber.mcelroy@gmail.com>
2024-09-30 20:54:18 -0600

    update js

--- earthquake/earthquake.js
+++ earthquake/earthquake.js
@@ -1,6 +1,6 @@
-var map = L.map('earthquakemap').setView([38, -95], 4);
-var basemapUrl = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png';
-var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+var map = L.map('earthquakemap').setView([20, -155], 4);
+var basemapUrl = 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png';
+var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'}).addTo(map);
 
 //add earthquake alerts layer
 var earthquakeFeedUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
@@ -15,7 +15,7 @@ $.getJSON(earthquakeFeedUrl, function(data) {
             } else return {color: alertColor};
         },
         onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.title, feature.properties.mag,);
+            layer.bindPopup('Magnitude: ' + feature.properties.mag + '</br>' + 'Location: ' + feature.properties.place + '</br>' + 'Time: ' + feature.properties.time);
         }
     }).addTo(map);
 });
