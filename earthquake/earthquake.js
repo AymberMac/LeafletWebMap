a6fd92b90c2c1ebc3d7834205b3b14efbf4cd13f
Aymber McElroy <aymber.mcelroy@gmail.com>
2024-10-01 00:19:20 -0600

    update all

--- earthquake/earthquake.js
+++ earthquake/earthquake.js
@@ -7,13 +7,18 @@ var earthquakeFeedUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summa
 $.getJSON(earthquakeFeedUrl, function(data) {
     L.geoJSON(data, {
         style: function(feature){
-            var alertColor = 'yellow';
-            if (feature.properties.mag >= 6.6) {
-                alertColor = 'red';
-            } else if (feature.properties.mag >= 3.3) {
-                alertColor = 'orange';
-            } else return {color: alertColor};
-        },
+            var magColor = 'Black';
+            if (feature.properties.mag > 9.0) alertColor = 'Indigo';
+            else if (feature.properties.mag > 8.0) alertColor = 'DarkMagenta';
+            else if (feature.properties.mag > 7.0) alertColor = 'DarkRed';
+            else if (feature.properties.mag > 6.0) alertColor = 'Red';
+            else if (feature.properties.mag > 5.0) alertColor = 'OrangeRed';
+            else if (feature.properties.mag > 4.0) alertColor = 'DarkOrange';
+            else if (feature.properties.mag > 3.0) alertColor = 'Orange';
+            else if (feature.properties.mag > 2.0) alertColor = 'Gold';
+            else if (feature.properties.mag > 1.0) alertColor = 'Yellow';
+            return { color: alertColor }
+          },
         onEachFeature: function(feature, layer) {
             layer.bindPopup('Magnitude: ' + feature.properties.mag + '</br>' + 'Location: ' + feature.properties.place + '</br>' + 'Time: ' + feature.properties.time);
         }
